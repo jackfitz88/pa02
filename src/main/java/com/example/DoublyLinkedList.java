36e6cc5cadeb253d23d50feaabb409a69b9c73df
jackfitz88 <fitzgeraldjack88@gmail.com>
2022-03-16 17:04:27 -0400

    Made Album class

---
+++ src/main/java/com/example/DoublyLinkedList.java
@@ -0,0 +1,101 @@
+package com.example;
+
+public class DoublyLinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    public DoublyLinkedList(){
+        this.head = null;
+        this.tail = null;
+    }
+
+    public Node append(Album data){
+        Node toAppend = new Node(data);
+
+        if (this.head == null) {
+            this.head = toAppend;
+            this.tail = toAppend;
+            this.head.prev = null;
+            this.tail.next = null;
+        }
+        else{
+            this.tail.next = toAppend;
+            toAppend.prev = this.tail;
+            this.tail = toAppend;
+            this.tail.next = null;
+        }
+        return toAppend;
+
+    }
+
+    // public Node insert(int location, Node data){
+        
+    // }
+
+    public Node delete(Album data){
+        Node toDelete = null;
+
+        if(this.head.data.compareTo(data) == 0){
+            toDelete = this.head;
+            this.head = this.head.next;
+
+            return toDelete;
+        }
+
+        Node current = this.head;
+
+        while(current.next != null){
+            if(current.next.data.compareTo(data) == 0){
+                toDelete = current.next;
+
+                current.next = toDelete.next;
+                toDelete.next = null;
+                return toDelete;
+            }
+
+            current = current.next;
+        }
+
+
+        return toDelete;
+    }
+    
+    public Node getIndex(Album data){
+        while(){
+            
+        }
+    }
+
+    public String toString(){
+        Node toPrint = this.head;
+
+        StringBuilder stringBuilder = new StringBuilder();
+        
+        while(toPrint != null){
+            stringBuilder.append(toPrint.data);
+            stringBuilder.append(" -> ");
+            toPrint = toPrint.next;
+        }
+
+        stringBuilder.append("Null");
+
+        return stringBuilder.toString();
+    }
+
+    public DoublyLinkedList partition(Album data){
+        Node current = this.head;
+        DoublyLinkedList dll = new DoublyLinkedList();
+
+        while(current.next != null){
+            if(current.data.compareTo(data) > 1){
+                dll.append(current.data);
+            }
+            current = current.next;
+        }
+
+        return dll;
+    }
+
+
+}
